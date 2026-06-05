035e5c600dbc5bb92ce33544e22914662d2fa11f
Brendan Ly <brendan.ly@sjsu.edu>
2026-06-05 16:10:48 -0700

    fix deployment

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar"
 import { useState } from "react";
 import RateLimitedUI from "../components/RateLimitedUI";
 import { useEffect } from "react";
-import axios from "axios";
+import api from "../lib/axios";
 import toast from "react-hot-toast";
 import NoteCard from "../components/NoteCard";
 
@@ -14,13 +14,13 @@ const HomePage = () => {
   useEffect(() => {
     const fetchNotes = async () => {
       try {
-        const res = await axios.get("http://localhost:5001/api/notes");
+        const res = await api.get("/notes");
         console.log(res.data)
         setNotes(res.data)
         setIsRateLimited(false)
       } catch (error) {
         console.log("Error fetching notes");
-        if (error.response.status === 429) {
+        if (error.response?.status === 429) {
           setIsRateLimited(true)
         } else {
           toast.error("Failed to load notes")
