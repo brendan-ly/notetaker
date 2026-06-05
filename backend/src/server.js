3a1d17f9d14fa04ca5eeea9a95897ad423cd553e
Brendan Ly <brendan.ly@sjsu.edu>
2026-06-05 15:40:10 -0700

    frontend design

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import notesRoutes from "./routes/notesRoutes.js";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
+import cors from "cors"
 
 dotenv.config();
 
@@ -11,8 +12,10 @@ const PORT = process.env.PORT || 5001;
 
 
 // middleware to parse JSON bodies
+app.use(cors({
+  origin:"http://localhost:5173",
+}));
 app.use(express.json());
-
 app.use(rateLimiter);
 
 app.use((req, res, next) => {
